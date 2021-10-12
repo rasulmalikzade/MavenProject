0a7c0bf534354981b2e044ae8ae3046dee083f2c
Rasul Malikzade <rasulmalikzade92@gmail.com>
2021-10-12 15:14:45 -0400

    sending new text

--- src/main/java/HelloMain.java
+++ src/main/java/HelloMain.java
@@ -2,5 +2,6 @@ public class HelloMain {
     public static void main(String[] args) {
         System.out.println("Hello Main");
         System.out.println("Hello feature developers");
+        System.out.println("Push new text");
     }
 }
